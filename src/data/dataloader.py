577fc47a9a4c42998138d3eceb5635230af9acc3
Tuan-Pham <weekend.2810@gmail.com>
2023-08-15 10:57:28 +0700

    feat: compatible dataloader for AdvanceQA

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -3,16 +3,14 @@ import math
 import random
 import sys
 sys.path.insert(0, r'./')
-
-import datasets
-import torch
-
-from typing import Optional, Dict, List, Union, Set
 from os.path import join
-from datasets import DatasetDict, load_dataset, concatenate_datasets
+from typing import Optional, Dict, List, Union, Set
+
+import torch
+import datasets
 from torch.utils.data import RandomSampler, SequentialSampler
 from torch.utils.data.dataloader import DataLoader, Dataset
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import AutoTokenizer
 
 from configs import AdvanceQAExample
 
@@ -48,7 +46,6 @@ class QADataloader:
                  test_file: Optional[Union[str, List[str]]]=None,
                  batch_size: int = 8,
                  seed: int = 42,
-                 translate: bool=False,
                  use_fast_tokenizer: bool=True,
                  max_train_samples: Optional[int] = None,
                  max_eval_samples: Optional[int] = None,
@@ -64,7 +61,6 @@ class QADataloader:
         self.val_file = val_file
         self.test_file = test_file
         self.batch_size = batch_size
-        self.translate = translate
 
         self.seed = seed
         self.generator = torch.Generator()
@@ -78,50 +74,37 @@ class QADataloader:
         dataloaders = {}
         if self.train_file is not None:
             print('\nLoading train datasets' + '.' * 10)
-            train_dataset = self.load_data('train', self.train_file)
-            if self.max_train_samples is not None:
-                train_dataset = train_dataset.select(range(self.max_train_samples))
+            train_dataset = self.load_data(self.train_file, self.max_train_samples)
             dataloaders['train'] = self.get_dataloader(train_dataset, shuffle_flag=True)
 
         if self.val_file is not None:
             print('\nLoading validation datasets' + '.' * 10)
-            eval_dataset = self.load_data('val', self.val_file)
-            if self.max_eval_samples is not None:
-                eval_dataset = eval_dataset.select(range(self.max_eval_samples))
+            eval_dataset = self.load_data(self.val_file, self.max_eval_samples)
             dataloaders['eval'] = self.get_dataloader(eval_dataset)
 
         if self.test_file is not None:
             print('\nLoading test datasets' + '.' * 10)
-            test_dataset = self.load_data('test', self.test_file)
-            if self.max_predict_samples is not None:
-                test_dataset = test_dataset.select(range(self.max_predict_samples))
+            test_dataset = self.load_data(self.test_file, self.max_predict_samples)
             dataloaders['test'] = self.get_dataloader(test_dataset)
 
         return dataloaders
 
-    def load_data(self, key: str, data_file: List[str]) -> DatasetDict:
+    def load_data(self, data_files: List[str], num_example: int=10000) -> AdvanceQa:
         """
         Loads a dataset from a file on disk and returns it as a dictionary of Dataset objects.
 
         Args:
-            key (str): The key to assign to the loaded dataset in the returned dictionary of Dataset objects.
-            data_file (Union[str, List[str]]): The path or paths to the data file(s) to load. If multiple is True, data_file
+            data_file (List[str]): The path or paths to the data file(s) to load. If multiple is True, data_file
                                                 should be a list of file paths. Otherwise, it should be a single file path.
-            mutiple (bool): A flag that indicates whether the data_file argument is a list of multiple file paths.
+            num_example (int): Max number of example in the final dataset that was loaded from each file
 
         Returns:
-            A dictionary of Dataset objects that represents the loaded dataset. If mutiple is True, the function
-            concatenates the datasets from the multiple files before returning them. Otherwise, it returns a single
-            dataset loaded from the data_file path.
+            A dataset object loaded from all data_files that was divided equally
         """
-
-        dataset_list = []
-        for file in data_file:
-            data_files = {key: file}
-            extension = file.split(".")[-1]
-            dataset_list.append(load_dataset(extension, data_files=data_files, split=key))
-        dataset = concatenate_datasets(dataset_list)
-        dataset.shuffle(self.seed)
+        if num_example:
+            dataset = AdvanceQa(json_file_paths=data_files, num_examples=num_example)
+        else:
+            dataset = AdvanceQa(json_file_paths=data_files)
         return dataset
 
     def dynamic_collate(self, batch):
@@ -136,16 +119,8 @@ class QADataloader:
             dict: A dictionary with the input IDs, attention masks, and target IDs with attention masks where tokens are padded,
             and the target IDs are masked to exclude padded values.
         """
-
-        inputs = [example[self.text_column] for example in batch]
-        targets = [example[self.target_column] for example in batch]
-
-        if self.translate:
-            inputs = translate_en2vi(inputs, self.device)
-            targets = translate_en2vi(targets, self.device)
-
-        print(inputs)
-        print(targets)
+        inputs = [example.get_example(is_training=self.train_file is not None)[self.text_column] for example in batch]
+        targets = [example.get_example(is_training=self.train_file is not None)[self.target_column] for example in batch]
 
         inp_tokens = self.tokenizer.batch_encode_plus(
             inputs,
@@ -176,37 +151,39 @@ class QADataloader:
         """
         sampler = RandomSampler(data_source=dataset,generator=self.generator) if shuffle_flag else SequentialSampler(dataset)
         dataloader = DataLoader(dataset,
-                                sampler= sampler,
+                                sampler=sampler,
                                 collate_fn=self.dynamic_collate,
                                 batch_size=self.batch_size,
-                                drop_last=True
+                                drop_last=True,
+                                pin_memory=True if torch.cuda.is_available() else False
                                 )
 
         return dataloader
 
 
 if __name__ == "__main__":
-    # dataloader_args = {
-    #     "model_name": "google/flan-t5-base",
-    #     "text_column": "prompt",
-    #     "target_column": "response",
-    #     "train_file": [r"C:\Users\Tuan Pham\Desktop\Study\SelfStudy\venv2\DPRMarxism_QA\src\data\storage\dummy\dummy.json"],
-    #     "batch_size": 2,
-    #     "seed": 42,
-    #     "translate": True,
-    #     "max_train_samples": 10
-    # }
-    #
-    # dataloaders = StateDataloader(**dataloader_args)
-    # dataloaders = dataloaders.__call__()
-    #
-    # for batch in iter(dataloaders['train']):
-    #     print(batch)
-    idx = random.randint(0, 2000)
-    qa_dataset = AdvanceQa(json_file_paths=[r"C:\Users\Tuan Pham\Desktop\Study\SelfStudy\venv2\DPRMarxism_QA\src\data\features\final_storge_converted\Train-v2.0\train-v2.0_converted.json"],
-                           num_examples=2000)
+    dataloader_args = {
+        "model_name": "google/mt5-base",
+        "text_column": "prompt",
+        "target_column": "target",
+        "train_file": [r"C:\Users\Tuan Pham\Desktop\Study\SelfStudy\venv2\DPRMarxism_QA\src\data\features\final_storge_converted\ELI5\ELI5_Parser_train_10_doc_translated.json",
+                       r"C:\Users\Tuan Pham\Desktop\Study\SelfStudy\venv2\DPRMarxism_QA\src\data\features\final_storge_converted\ELI5\ELI5_Parser_translated_val.json"],
+        "batch_size": 8,
+        "seed": 42,
+        "max_train_samples": 450
+    }
+
+    idx = random.randint(0, 400)
+    qa_dataset = AdvanceQa(json_file_paths=[r"C:\Users\Tuan Pham\Desktop\Study\SelfStudy\venv2\DPRMarxism_QA\src\data\features\final_storge_converted\ELI5\ELI5_Parser_train_10_doc_translated.json"],
+                           num_examples=400)
     print(qa_dataset[idx])
     print(qa_dataset[idx].get_dict)
     print(qa_dataset[idx].get_dict_str)
     print(qa_dataset[idx].get_example(is_training=True))
+
+    qa_dataloader = QADataloader(**dataloader_args)
+    qa_dataloader_instance = qa_dataloader.__call__()
+    for data in iter(qa_dataloader_instance['train']):
+        print(qa_dataloader.tokenizer.decode(data['input_ids'][0], skip_special_tokens=True))
+        break
 
